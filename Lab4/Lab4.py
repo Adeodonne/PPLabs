74f715b2988d8b366b0b1289bd0435bf5a79870a
Adeodonne <bbozhik284@gmail.com>
2022-09-28 04:40:03 +0300

    make flask project and waitress server

--- Lab4/Lab4.py
+++ Lab4/Lab4.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, make_response
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/api/v1/hello-world-2', methods=['GET'])
+    def helloorld():
+        return 'Hello World 2!'
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World123!'
-
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
+
+#waitress-serve --host 127.0.0.1 --port 8081 --call "Lab4:create_app"
+#http://127.0.0.1:8081/api/v1/hello-world-2
