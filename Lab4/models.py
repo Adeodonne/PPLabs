3f68954ab33deef4aec645508736a0da9f16cc10
Adeodonne <bbozhik284@gmail.com>
2022-11-12 12:56:13 +0200

    add Lab 6 and 7

---
+++ Lab4/models.py
@@ -0,0 +1,195 @@
+from uuid import UUID
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base, scoped_session
+from sqlalchemy_serializer import SerializerMixin
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+engine = create_engine('mysql+pymysql://root:31012004!Q@127.0.0.1:3306/deliveryserviceupdated')
+SessionFactory = sessionmaker(bind=engine)
+Session = scoped_session(SessionFactory)
+Base = declarative_base()
+Base.metadata.create_all(bind=engine)
+session = Session()
+
+class CustomSerializerMixin(SerializerMixin):
+	serialize_types = (
+		(UUID, lambda x: str(x)),
+	)
+
+class Country(Base, CustomSerializerMixin):
+	__tablename__ = 'country'
+
+	serialize_only = {'id', 'countryName'}
+
+	id = Column('id', Integer, primary_key = True, autoincrement = True, nullable = False)
+	countryName = Column('countryName', String(45), nullable = False)
+
+class State(Base, CustomSerializerMixin):
+	__tablename__ = 'state'
+
+	serialize_only = {'id', 'stateName', 'idCountry'}
+
+	id = Column('id', Integer, primary_key = True, autoincrement = True, nullable = False)
+	stateName = Column('stateName', String(45), nullable = False)
+	idCountry = Column('idCountry', Integer, ForeignKey(Country.id), nullable = False)
+	country = relationship(Country, backref='state', lazy='joined')
+
+class City(Base, CustomSerializerMixin):
+	__tablename__ = 'city'
+
+	serialize_only = {'id', 'cityName', 'idState'}
+
+	id = Column('id', Integer, primary_key = True, autoincrement = True, nullable = False)
+	cityName = Column('cityName', String(45), nullable = False)
+	idState = Column('idState', Integer, ForeignKey(State.id), nullable = False)
+	state = relationship(State, backref='city', lazy='joined')
+
+class User(Base, CustomSerializerMixin):
+	__tablename__ = 'user'
+
+	serialize_only = {'id', 'fullName', 'address', 'idCity', "phoneNumber", "password", "role"}
+
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+	fullName = Column('fullName', String(60), nullable=False)
+	address = Column('address', String(45), nullable=False)
+	idCity = Column('idCity', Integer, ForeignKey(City.id), nullable=False)
+	city = relationship(City, backref='user', lazy='joined')
+	phoneNumber = Column('phoneNumber', String(13), nullable=False)
+	password = Column('password', String(60), nullable=False)
+	role = Column('role', Integer, nullable=False)
+
+class UserSchema(SQLAlchemyAutoSchema):
+	class Meta:
+		model = User
+		include_relationships = True
+		load_instance = True
+		include_fk = True
+
+class PostOffice(Base, CustomSerializerMixin):
+	__tablename__ = 'postoffice'
+
+	serialize_only = {'id', 'name', 'idCity', 'address'}
+
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+	name = Column('name', String(45), nullable=False)
+	idCity = Column('idCity', Integer, ForeignKey(City.id), nullable=False)
+	city = relationship(City, backref='postoffice', lazy='joined')
+	address = Column('address', String(45), nullable=False)
+
+class ProductType(Base, CustomSerializerMixin):
+	__tablename__ = 'producttype'
+
+	serialize_only = {'id', 'typeName'}
+
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+	TypeName = Column('typeName', String(45), nullable=False)
+
+class ShipmentType(Base, CustomSerializerMixin):
+	__tablename__ = 'shipmenttype'
+
+	serialize_only = {'id', 'name'}
+
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+	name = Column('name', String(45), nullable=False)
+
+class PaymentType(Base, CustomSerializerMixin):
+	__tablename__ = 'paymenttype'
+
+	serialize_only = {'id', 'paymentTypeName'}
+
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+	paymentTypeName = Column('paymentTypeName', String(45), nullable=False)
+
+class Seller(Base, CustomSerializerMixin):
+	__tablename__ = 'seller'
+
+	serialize_only = {'id', 'weight', 'volume', 'idPostOfficeSender', "sendTime", "description", "approximatelyPrice"}
+
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+	weight = Column('weight', Integer, nullable=False)
+	volume = Column('volume', Integer, nullable=False)
+
+	idPostOfficeSender = Column('idPostOfficeSender', Integer, ForeignKey(PostOffice.id), nullable=False)
+	postOfficeSender = relationship(PostOffice, backref='Seller', lazy='joined')
+
+	sendTime = Column('sendTime', DATETIME, nullable=False)
+	description = Column('description', String(200))
+	approximatelyPrice = Column('approximatelyPrice', Integer)
+
+class SellerSchema(SQLAlchemyAutoSchema):
+	class Meta:
+		model = Seller
+		include_relationships = True
+		load_instance = True
+		include_fk = True
+
+class Shipment(Base, CustomSerializerMixin):
+	__tablename__ = 'shipment'
+
+	serialize_only = {'id', 'idUser', 'idShipmentType', 'reciveTime', "idPostOffice", "idProductType", "idSeller"}
+
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+
+	idUser = Column('idUser', Integer, ForeignKey(User.id), nullable=False)
+	user = relationship(User, backref='shipment', lazy='joined')
+
+	idShipmentType = Column('idShipmentType', Integer, ForeignKey(ShipmentType.id), nullable=False)
+	shipmentType = relationship(ShipmentType, backref='shipment', lazy='joined')
+
+	reciveTime = Column('reciveTime', DATETIME)
+
+	idPostOffice = Column('idPostOffice', Integer, ForeignKey(PostOffice.id), nullable=False)
+	postOffice = relationship(PostOffice, backref='shipment', lazy='joined')
+
+	idProductType = Column('idProductType', Integer, ForeignKey(ProductType.id), nullable=False)
+	productType = relationship(ProductType, backref='shipment', lazy='joined')
+
+	idSeller = Column('idSeller', Integer, ForeignKey(Seller.id), nullable=False)
+	seller = relationship(Seller, backref='shipment', lazy='joined')
+
+class ShipmentSchema(SQLAlchemyAutoSchema):
+	class Meta:
+		model = Shipment
+		include_relationships = True
+		load_instance = True
+		include_fk = True
+
+class ShipmentStatusName(Base, CustomSerializerMixin):
+	__tablename__ = 'shipmentstatusname'
+
+	serialize_only = {'id', 'statusName'}
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+	statusName = Column('statusName', String(45), nullable=False)
+
+class ShipmentStatus(Base, CustomSerializerMixin):
+	__tablename__ = 'shipmentstatus'
+
+	serialize_only = {'id', 'idShipmentStatusName', 'idShipment', 'statusTime', "details"}
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+
+	idShipmentStatusName = Column('idShipmentStatusName', Integer, ForeignKey(ShipmentStatusName.id), nullable=False)
+	shipmentStatusName = relationship(ShipmentStatusName, backref='shipmentstatus', lazy='joined')
+
+	idShipment = Column('idShipment', Integer, ForeignKey(Shipment.id), nullable=False)
+	shipment = relationship(Shipment, backref='shipmentstatus', lazy='joined')
+
+	statusTime = Column('statusTime', DATETIME, nullable=False)
+
+	details = Column('details', String(200))
+
+class PaymentDetails(Base, CustomSerializerMixin):
+	__tablename__ = 'paymentdetails'
+
+	serialize_only = {'id', 'price', 'idPaymentType', 'paymentTime', "idShipment"}
+	id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+	price = Column('price', Integer, nullable=False)
+
+	idPaymentType = Column('idPaymentType', Integer, ForeignKey(PaymentType.id), nullable=False)
+	paymentType = relationship(PaymentType, backref='paymentdetails', lazy='joined')
+
+	paymentTime = Column('paymentTime', DATETIME)
+
+	idShipment = Column('idShipment', Integer, ForeignKey(Shipment.id), nullable=False)
+	shipment = relationship(Shipment, backref='paymentdetails', lazy='joined')
+
+Base.metadata.create_all(bind=engine)
---
+++ Lab4/routes.py
@@ -0,0 +1,148 @@
+from flask import Flask, request, render_template
+from waitress import serve
+from models import *
+from marshmallow import validate, ValidationError
+from flask_bcrypt import Bcrypt
+import json
+
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+@app.route('/api/v1/hello-world-2', methods=['GET'])
+def helloorld():
+    return 'Hello World 2!'
+
+@app.route('/', methods=['GET'])
+def helloworld():
+    return 'Hello World 2!'
+
+@app.route('/user/', methods=['GET']) # http://0.0.0.0:8080/user/?id=0
+def FindUser():
+    args = request.args
+    id = args.get('id')
+    users = Session.query(User).filter(User.id == id)
+    return json.dumps([i.to_dict() for i in users])
+
+"""
+Example for post
+{
+    "id": 1,
+    "fullName": "User1 Name",
+    "address": "User1 Address",
+    "idCity": 0,
+    "phoneNumber": "+380111111111",
+    "password": "User1Password",
+    "role": 0  
+}
+"""
+@app.route('/user', methods=['POST'])
+def NewUser():
+    args = request.get_json()
+    try:
+        user_schema = UserSchema()
+        user = user_schema.load(args, session=session)
+        #user.Password = bcrypt.generate_password_hash(user.password)
+        session.add(user)
+        session.commit()
+        return user_schema.dump(user)
+    except ValidationError as err:
+        return json.dumps({'success':False, 'status':400, "error":str(err)}), 400, {'ContentType':'application/json'}
+
+@app.route('/user/', methods=['DELETE']) # http://0.0.0.0:8080/user/?id=1
+def DeleteUser():
+    session = Session()
+    args = request.args
+    id = args.get('id')
+    session.query(User).filter(User.id == id).delete()
+    session.commit()
+    session.close()
+    return "User deleted"
+
+@app.route('/shipment/', methods=['GET'])
+def FindShipment():
+    args = request.args
+    id = args.get('id')
+    shipments = Session.query(Shipment).filter(Shipment.id == id)
+    return json.dumps([i.to_dict() for i in shipments])
+
+"""
+Example for post
+{
+    "id": 0,
+    "weight" : 100,
+    "volume": 100,
+    "idPostOfficeSender": 0,
+    "sendTime": "2011-04-15 00:03:20",
+    "description": "Description0",
+    "approximatelyPrice" : 0   
+}
+"""
+@app.route('/shipment', methods=['POST'])
+def CreateShipment():
+    args = request.get_json()
+    try:
+        shipmentSchema = ShipmentSchema()
+        shipment = shipmentSchema.load(args, session=session)
+        session.add(shipment)
+        session.commit()
+        return shipmentSchema.dump(shipment)
+    except ValidationError as err:
+        return json.dumps({'success':False, 'status':400, "error":str(err)}), 400, {'ContentType':'application/json'}
+
+@app.route('/shipment/', methods=['DELETE'])
+def DeleteShipment():
+    session = Session()
+    args = request.args
+    id = args.get('id')
+    session.query(Shipment).filter(Shipment.id == id).delete()
+    session.commit()
+    session.close()
+    return "Shipment deleted"
+
+@app.route('/seller/', methods=['GET'])
+def FindSeller():
+    args = request.args
+    id = args.get('id')
+    sellers = Session.query(Seller).filter(Seller.id == id)
+    return json.dumps([i.to_dict() for i in sellers])
+
+"""
+Example for post
+{
+    "id": 0,
+    "idUser" : 0,
+    "idShipmentType": 0,
+    "reciveTime": "2011-04-15 00:03:20",
+    "idPostOffice" : 0,
+    "idProductType": 0,
+    "idSeller" : 0   
+}
+"""
+@app.route('/seller', methods=['POST'])
+def CreateSeller():
+    args = request.get_json()
+    try:
+        sellerSchema = SellerSchema()
+        shipment = sellerSchema.load(args, session=session)
+        session.add(shipment)
+        session.commit()
+        return sellerSchema.dump(shipment)
+    except ValidationError as err:
+        return json.dumps({'success':False, 'status':400, "error":str(err)}), 400, {'ContentType':'application/json'}
+
+@app.route('/seller/', methods=['DELETE'])
+def DeleteSeller():
+    session = Session()
+    args = request.args
+    id = args.get('id')
+    session.query(Seller).filter(Seller.id == id).delete()
+    session.commit()
+    session.close()
+    return "Seller deleted"
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
+    serve(app)
+
+#waitress-serve routes:app
+#http://127.0.0.1:8081/api/v1/hello-world-2
